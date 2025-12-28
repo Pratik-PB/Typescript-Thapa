dd8e13e851c51e9b6c9bdca7a97a3dc28e07fd4a
pratikbabar97 <pratikbabar2199@gmail.com>
2025-12-28 17:55:47 +0530

    Addition and multiply feature added

---
+++ addition.ts
@@ -0,0 +1,13 @@
+
+
+
+
+const addition = () => {
+
+    const num: number = 10;
+    
+    const num2: number =10;
+
+    console.log(num+num2);
+    
+}
---
+++ multiply.ts
@@ -0,0 +1,12 @@
+
+
+
+
+
+const multiply=(num : number): void=>{
+      num = num * 2;
+        console.log(`The multiplied value is: ${num}`);
+}
+
+
+multiply(10);
